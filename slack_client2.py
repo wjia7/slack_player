ac4babdbc3f273dc77441ad695b0abfec634adb6
William Jia <wjia@WJia-mac-3.ott.signiant.com>
2019-09-06 14:06:56 -0400

    add slack_client_v2 not work yet

--- slack_client2.py
+++ slack_client2.py
@@ -14,8 +14,8 @@ SLACK_BOT_TOKEN=global_vars.SLACK_BOT_TOKEN
 # bot channel
 SLACK_DEFAULT_CHANNEL=global_vars.SLACK_DEFAULT_CHANNEL
 # instantiate Slack client
-#slack_client = slack.WebClient(SLACK_BOT_TOKEN)
-rtmclient = slack.RTMClient(SLACK_BOT_TOKEN)
+slack_client = slack.WebClient(SLACK_BOT_TOKEN)
+rtmclient = slack.RTMClient(token=SLACK_BOT_TOKEN)
 # starterbot's user ID in Slack: value is assigned after the bot starts up
 starterbot_id = None
 
@@ -48,16 +48,13 @@ def parse_direct_mention(message_text):
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
 
-def handle_command(command, channel, pl, current_song):
-    """
-        Executes bot command if the command is known
-    """
-    # Default response is help text for the user
-    default_response = "Not sure what you mean. Try *{}*.".format("play a song")
+@slack.RTMClient.run_on(event='message')
+def say_message(**payload):
+    print("what is here",payload)
+    data = payload['data']
+    command = data['text']
 
-    # Finds and executes the given command, filling in response
-    response = None
-    # This is where you start to implement more commands!
+    global CURRENT_SONG
 
     if command.startswith("request"):
 
@@ -65,161 +62,94 @@ def handle_command(command, channel, pl, current_song):
         search_keywords = command[8:]
         print(search_keywords)
 
+        user = data['user']
         # if current_song.is_playing() and current_song.get_creator() != "auto":
-        if current_song.is_playing():
+        if CURRENT_SONG.is_playing():
             # if there is a song playing wait till it's finish
             song_data = pl.add_song(search_keywords)
             response = "Ok I have added {} to queue".format(song_data['name'])
         else:
             # else play that song.
-            current_song.stop_audio()
+            CURRENT_SONG.stop_audio()
             pl.add_song(search_keywords)
             song_data = pl.get_song()
             print(song_data)
-            current_song = music_player2.YoutubeAudio(song_data)
-            current_song.play_audio()
-            song_name = filter_title(song_data['name'])
-            response = "Now playing {}".format(song_name)
+            CURRENT_SONG = music_player2.YoutubeAudio(song_data)
+            CURRENT_SONG.play_audio()
+            response = "Now playing {}".format(song_data['name'])
 
         print("current play list",pl.get_playlist)
+        webclient = payload['web_client']
+        webclient.chat_postMessage(channel=SLACK_DEFAULT_CHANNEL, text=response)
 
-    @slack.RTMClient.run_on(event='message')
-    def say_request(**payload):
-        command = payload['data']
-        if command.startswith("request"):
-            new_c = current_song
-            print(command)
-            search_keywords = command[8:]
-            print(search_keywords)
+    if command.startswith("nowplaying"):
+        response = "currently playing: {0}".format(CURRENT_SONG.get_name())
+        webclient = payload['web_client']
+        webclient.chat_postMessage(channel=SLACK_DEFAULT_CHANNEL, text=response)
 
-            # if current_song.is_playing() and current_song.get_creator() != "auto":
-            if current_song.is_playing():
-                # if there is a song playing wait till it's finish
-                song_data = pl.add_song(search_keywords)
-                response = "Ok I have added {} to queue".format(song_data['name'])
-            else:
-                # else play that song.
-                current_song.stop_audio()
-                pl.add_song(search_keywords)
-                song_data = pl.get_song()
-                print(song_data)
-                current_song = music_player2.YoutubeAudio(song_data)
-                current_song.play_audio()
-                song_name = filter_title(song_data['name'])
-                response = "Now playing {}".format(song_name)
-
-            print("current play list", pl.get_playlist)
-
-            webclient = payload['web_client']
-            webclient.chat_postMessage(
-
-            )
-
-
-    elif command.startswith("nowplaying"):
-        response = "currently playing: {0}".format(current_song.get_name())
-
-    elif command.startswith("skip"):
-        current_song.stop_audio()
+    if command.startswith("skip"):
+        CURRENT_SONG.stop_audio()
         song_data = pl.get_song()
         print(song_data['name'])
         current_song = music_player2.YoutubeAudio(song_data)
         current_song.play_audio()
-        song_name = filter_title(current_song.get_name())
-        response = "Now playing: {0}".format(song_name)
+        response = "Now playing: {0}".format(current_song.get_name())
+        webclient = payload['web_client']
+        webclient.chat_postMessage(channel=SLACK_DEFAULT_CHANNEL, text=response)
 
-    elif command.startswith("nextup"):
+    if command.startswith("nextup"):
         song_list = pl.get_next_info(limit=5)
         s_list = ""
         for s in song_list:
             s_list = s_list + "{}\n".format(s['name']+"\n")
         response = "Coming up: \n" + s_list
+        webclient = payload['web_client']
+        webclient.chat_postMessage(channel=SLACK_DEFAULT_CHANNEL, text=response)
 
-    elif command.startswith("help"):
+    if command.startswith("help"):
         response = "You can ask me the following: \n" \
                    "request - request a song \n" \
                    "skip - skip to the next track \n" \
                    "nowplaying - report back what is currently playing \n" \
                    "nextup - report back what is coming up \n" \
                    "stop - close the app \n"
+        webclient = payload['web_client']
+        webclient.chat_postMessage(channel=SLACK_DEFAULT_CHANNEL, text=response)
 
-    elif command.startswith("stop"):
+    if command.startswith("stop"):
         response = "stop music"
+        webclient = payload['web_client']
+        webclient.chat_postMessage(channel=SLACK_DEFAULT_CHANNEL, text=response)
         sys.exit()
 
 
-    # Sends the response back to the channel
-    print("internal channel", channel)
-    slack_client.api_call("chat.postMessage",channel=channel, text=response)
-    return current_song, channel
+# start the song
+pl = music_player2.Playlist()
+song_data = pl.get_song()
+print(song_data['name'])
+CURRENT_SONG = music_player2.YoutubeAudio(song_data)
+# play the first auto song right away.
+CURRENT_SONG.play_audio()
+response = "Now playing: {0}".format(CURRENT_SONG.get_name())
+slack_client.chat_postMessage(channel=SLACK_DEFAULT_CHANNEL, text=response)
 
+# start the interaction
+rtmclient.start()
 
-def filter_title(song_n):
-    # remove 1080p, video from song name
-    song_name = song_n.lower()
-    song_name = song_name.replace("video", '')
-    song_name = song_name.replace("music", '')
-    song_name = song_name.replace("official", '')
-    song_name = song_name.replace("1080p", '')
-    song_name = song_name.replace("lyrics", '')
-    song_name = song_name.replace("(", '')
-    song_name = song_name.replace(")", '')
-    song_name = song_name.replace("]", '')
-    song_name = song_name.replace("[", '')
-    song_name = song_name.replace(",", '')
-    re.sub(r'\s+', ' ', song_name)
-    print("filter", song_name)
-    return song_name
-
-if __name__ == "__main__":
-
-    if slack_client.rtm_connect(with_team_state=False):
-        print("Starter Bot connected and running!")
-        # Read bot's user ID by calling Web API method `auth.test`
-        starterbot_id = slack_client.api_call("auth.test")["user_id"]
-
-        # play the first auto song right away.
-        pl = music_player2.Playlist()
-        song_data = pl.get_song()
-        print(song_data['name'])
-        current_song = music_player2.YoutubeAudio(song_data)
-        current_song.play_audio()
-        # set default channel to SLACK_DEFAULT_CHANNEL
-        channel = SLACK_DEFAULT_CHANNEL
-        song_name = filter_title(current_song.get_name())
-        response = "Now playing: {0}".format(song_name)
-        slack_client.api_call("chat.postMessage", channel=channel, text=response)
-
-        while True:
-            command, channel = parse_bot_commands(slack_client.rtm_read())
-
-            if command:
-                current_song, this_channel = handle_command(command, channel, pl, current_song)
-
-            if not channel:
-                this_channel = SLACK_DEFAULT_CHANNEL
-
-            # when no song is playing play auto song after 3 seconds
-            if not current_song.is_playing():
-                time.sleep(3)
-                if not current_song.is_playing():
-                    print("here")
-                    current_song.stop_audio()
-                    song_data = pl.get_song()
-                    print(song_data['name'])
-                    current_song = music_player2.YoutubeAudio(song_data)
-                    current_song.play_audio()
-                    song_name = filter_title(current_song.get_name())
-                    response = "Now playing: {0}".format(song_name)
-                    print("channel: ", this_channel)
-                    slack_client.api_call("chat.postMessage", channel=this_channel, text=response)
-
-            time.sleep(RTM_READ_DELAY)
-    else:
-        print("Connection failed. Exception traceback printed above.")
+# #TODO find a way to check when the song is finished and start playing a new song. Below doesn't work
+# while True:
+#     print("song status: ",CURRENT_SONG.is_playing())
+#     if not CURRENT_SONG.is_playing():
+#         time.sleep(3)
+#         CURRENT_SONG.stop_audio()
+#         song_data = pl.get_song()
+#         print(song_data['name'])
+#         current_song = music_player2.YoutubeAudio(song_data)
+#         current_song.play_audio()
+#         response = "Now playing: {0}".format(current_song.get_name())
+#         print("channel: ", SLACK_DEFAULT_CHANNEL)
+#         slack_client.chat_postMessage(channel=SLACK_DEFAULT_CHANNEL, text=response)
 
 
 
-# *** created using https://www.fullstackpython.com/blog/build-first-slack-bot-python.html
-# TODO recovery from failure on play. bundle all play into a function with try catch and restart?
-# TODO maybe if song length is longer than 8 min skip it
+#TODO need a way to pass the state of the song across state of slack client
